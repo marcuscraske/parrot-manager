12737c266b4f550e595b9feafb1ebf9fe8805722
limpygnome <limpygnome@gmail.com>
2016-11-20 19:48:57 +0000

    Progress save.

---
+++ src/main/java/com/limpygnome/parrot/service/RuntimeService.java
@@ -0,0 +1,22 @@
+package com.limpygnome.parrot.service;
+
+import javafx.scene.web.WebView;
+import jdk.nashorn.api.scripting.JSObject;
+
+/**
+ * Created by limpygnome on 17/11/16.
+ */
+public class RuntimeService
+{
+    private WebView webView;
+
+    public void setWebView(WebView webView)
+    {
+    }
+
+    public void exposeJsObject(String variableName, Object object)
+    {
+        JSObject obj =
+    }
+
+}
