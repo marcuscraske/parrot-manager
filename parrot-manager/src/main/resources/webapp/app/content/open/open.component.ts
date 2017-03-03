d77c71331205da3a58e90a886f89acdf53e3e345
limpygnome <limpygnome@gmail.com>
2017-03-03 17:22:15 +0000

    Recent files - most recent is now shown on startup.

--- parrot-manager/src/main/resources/webapp/app/content/open/open.component.ts
+++ parrot-manager/src/main/resources/webapp/app/content/open/open.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { DatabaseService } from 'app/service/database.service'
 import { RuntimeService } from 'app/service/runtime.service'
 import { RecentFileService } from 'app/service/recentFile.service'
+import { SettingsService } from 'app/service/settings.service'
 import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'open.component.html',
-    providers: [DatabaseService, RuntimeService, RecentFileService]
+    providers: [DatabaseService, RuntimeService, RecentFileService, SettingsService]
 })
 export class OpenComponent {
 
+    static isStartup : boolean = true;
     errorMessage: string;
     recentFiles: any;
 
@@ -18,12 +20,29 @@ export class OpenComponent {
         private databaseService: DatabaseService,
         private runtimeService: RuntimeService,
         private recentFileService: RecentFileService,
+        private settingsService: SettingsService,
         private router: Router
     ) { }
 
     ngOnInit()
     {
+        // Fetch recently opened files
         this.recentFiles = this.recentFileService.fetch();
+
+        // Open database on startup if there's recent files and enabled...
+        if (OpenComponent.isStartup && this.recentFiles.length > 0)
+        {
+            var settings = this.settingsService.fetch();
+            var isEnabled = settings.recentFilesOpenLastOnStartup;
+
+            if (isEnabled)
+            {
+                this.openFile(this.recentFiles[0].getFullPath());
+            }
+        }
+
+        // Set startup flag, hence the above will only occur on startup
+        OpenComponent.isStartup = false;
     }
 
     chooseDatabaseFile() : void
